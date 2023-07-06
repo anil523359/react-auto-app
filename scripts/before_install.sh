55972b1fc01fe5193136f4373f499eb313ee8794
opsroot-at-696328474751 <anil.lko7171@gmail.com>
2023-07-06 11:09:01 +0530

    commit

---
+++ scripts/before_install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#_Change_Working_Directory
+cd /home/ubuntu/server
+
+#_Update_&_Set_Node_Version
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+
+#_Download_Node_&NPM
+sudo apt -y install nodejs npm
+
+#_Download_PM2
+npm install pm2@latest -g
